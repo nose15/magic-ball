bb343b722f3a3b54de9d235ca7b40b549a33fee7
kutasz <lukasz.janczak.pl@gmail.com>
2023-10-04 13:17:04 +0200

    Added closing on function call

--- src/api/gpt_client.py
+++ src/api/gpt_client.py
@@ -1,10 +1,28 @@
 from . import http_methods
+import time
 
 
 class GPTClient:
     messages = []
+    functions = [
+        {
+            "name": "close_chat",
+            "description": "Close the application when the user ends the conversation",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "message": {
+                        "type": "string",
+                        "description": "The goodbye message from the bot",
+                    }
+                },
+                "required": ["message"],
+            },
+        }
+    ]
+    conversation = False
 
-    def __init__(self, model, system_prompt="You are a chatbot"):
+    def __init__(self, model: str, system_prompt="You are a chatbot"):
         self.model = model
         system_message = {
             "role": "system",
@@ -12,7 +30,35 @@ class GPTClient:
         }
         self.messages.append(system_message)
 
-    def complete_chat(self, message):
+        self.function_names = {"close_chat": self.close_chat}
+
+    def run_conversation(self):
+        self.conversation = True
+
+        gpt_response = self.complete_chat("Witaj magiczna kulo")
+        print("Magiczna Kula:", gpt_response, "\n")
+
+        while self.conversation:
+            prompt = input("Wpisz wiadomosc: ")
+            gpt_response = self.complete_chat(prompt)
+            print("\nMagiczna Kula:", gpt_response, "\n")
+
+        print("*Kula znika*\n")
+        input()
+
+    def complete_chat(self, message: str) -> str:
+        response = self.send_message(message)
+
+        if "content" in response.keys():
+            return response["content"]
+
+        func_name = response["name"]
+        func = self.function_names[func_name]
+        func_response = func(response["arguments"]["message"])
+
+        return func_response
+
+    def send_message(self, message: str) -> dict:
         user_message = {
             "role": "user",
             "content": message
@@ -22,11 +68,23 @@ class GPTClient:
 
         payload = {
             "model": self.model,
-            "messages": self.messages
+            "messages": self.messages,
+            "functions": self.functions
         }
 
         response = http_methods.http_post(payload)
 
         self.messages.append(response)
 
-        return response["content"]
+        return response
+
+    def close_chat(self, message: str) -> str:
+        self.conversation = False
+        return message
+
+
+
+
+
+
+
--- src/api/http_methods.py
+++ src/api/http_methods.py
@@ -1,11 +1,12 @@
 import os
 import requests
+import json
 
 API_KEY = ""
 URL = "https://api.openai.com/v1/chat/completions"
 
 
-def http_post(payload: dict) -> str:
+def http_post(payload: dict) -> dict:
     headers = {
         "Authorization": f"Bearer {API_KEY}",
         "Content-Type": "application/json"
@@ -16,6 +17,12 @@ def http_post(payload: dict) -> str:
 
         if response.ok:
             response_json = response.json()
+
+            if "function_call" in response_json["choices"][0]["message"].keys():
+                function_args = response_json["choices"][0]["message"]["function_call"]["arguments"]
+                response_json["choices"][0]["message"]["function_call"]["arguments"] = json.loads(function_args)
+                return response_json["choices"][0]["message"]["function_call"]
+
             return response_json["choices"][0]["message"]
         else:
             raise Exception("Http POST Exception", response.status_code, response.reason)
